b34e61b7a2f37acce655621d8f1aca3843afc1e2
avinaguriifm <115398292+avinaguriifm@users.noreply.github.com>
2022-10-12 14:31:45 +0530

    Update div.java

--- div.java
+++ div.java
@@ -1,6 +1,7 @@
 public class div{
     public static void main(String []args) {
         System.out.println("division is occured");
+        System.out.println("my name is avinash");
         
     }
 }
